c06f532017a190b10277256e3d9c0a23bb4bb50a
Ryan Aquilina <ryanaquilina001@gmail.com>
2016-11-17 13:17:57 +0100

    Create Explicit Intent

---
+++ app/src/main/java/com/vogella/sterlingryan/intents/MainActivity.java
@@ -0,0 +1,27 @@
+package com.vogella.sterlingryan.intents;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        EditText text = (EditText) findViewById(R.id.inputforintent);
+        String value = text.getText().toString();
+
+        //TODO create new intent
+        Intent intent = new Intent(this, ResultActivity.class);
+
+        //TODO start second activity
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/vogella/sterlingryan/intents/ResultActivity.java
@@ -0,0 +1,20 @@
+package com.vogella.sterlingryan.intents;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by SterlingRyan on 17/11/2016.
+ */
+
+public class ResultActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
